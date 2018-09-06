5f8e2a7a838d075b4ab19cda5a6848f0020d473b
ricardopg <ricardopg@yandex.com>
2018-09-06 13:55:00 -0500

    Add project files.

---
+++ AspNetDependencyLifetime/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetDependencyLifetime.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
